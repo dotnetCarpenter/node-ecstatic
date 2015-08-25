0767fb0b1b4753cba90430a8660326de594eb5ce
dotnetCarpenter <jon.ronnenberg@gmail.com>
2015-08-25 03:50:00 +0300

    dump headers

--- bin/dump.js
+++ bin/dump.js
@@ -2,37 +2,43 @@
 
 "use strict";
 
-var request   = require('request'),
-    ecstatic  = require('../lib/ecstatic'),
-    http      = require('http'),
-    port      = 3000,
-    root      = __dirname + '/public',
-    uri       = 'http://localhost:' + port + 'compress/foo.js.gz',
-    server    = http.createServer(
-                  ecstatic({
-                    root: root,
-                    gzip: true,
-                    autoIndex: true,
-                    showDir: true
-                  })
-                )
+var request           = require('request')
+  , ecstatic          = require('../lib/ecstatic')
+  , http              = require('http')
+  , path              = require('path')
+  , port              = 9000
+  , root              = path.join(__dirname, '../', '/public')
+  , uri               = 'http://localhost:' + port + '/compress/foo.js.gz'
+  , whenServerIsReady = []
+  , server            = http.createServer(
+                          ecstatic({
+                            root: root,
+                            gzip: true,
+                            autoIndex: true,
+                            showDir: true
+                          })
+                        )
+
+server.listen(port, function() {
+  whenServerIsReady.forEach(function(fn) { fn() })
+})
 
 function getRequest() {
-  server.listen(port, function() {
-    request.get({
-      uri: uri
-    , followRedirect: false
-    , headers: { 'if-modified-since': Date.now() }
-    }, function (err, res, body) {
-      if (err) console.log(err), process.exit(1)
+  request.get({
+    uri: uri
+  , followRedirect: false
+  , headers: { 'if-modified-since': Date.now() }
+  }, function (err, res, body) {
+    if (err) console.log(err), process.exit(1)
 
-      console.dir(res)
-      console.log("*******************************************\r\n")
-      console.dir(res.headers)
+  //  console.dir(res)
+    console.log("*******************************************\r\n")
+    console.dir(res.headers)
 
-      server.close()
-    });
-  })
+    server.close()
+  });
 }
 
-[1,2].forEach(getRequest)
+[1].forEach(function() {
+  whenServerIsReady.push(getRequest)
+})
