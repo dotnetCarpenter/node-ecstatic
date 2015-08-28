1d9dc087c93c2fd9abf7a114687d2a0f9d81ba4b
dotnetCarpenter <jon.ronnenberg@gmail.com>
2015-08-29 02:54:34 +0300

    added probe class

--- bin/dump.js
+++ bin/dump.js
@@ -10,7 +10,6 @@ var request           = require('request')
   , root              = path.join(__dirname, '../', '/public')
   , url               = 'http://localhost:' + port
   , uris              = [url + '/compress/foo.js.gz', url + '/a.txt', url + '/compress/foo.js']
-  , whenServerIsReady = []
   , server            = http.createServer(
                           ecstatic({
                             root: root,
@@ -19,9 +18,37 @@ var request           = require('request')
                             showDir: true
                           })
                         )
+  , probe = {
+      probes: []
+    , add: function(fn) {
+        this.probes.push(fn)
+        return this
+      }
+    , times: function(num) {
+        num = Number(num) - 1
+        var lastFn = this.last()
+        for(var start = 0, end = start + num; start < end; start++) {
+          this.probes.push(lastFn)
+        }
+        return this
+      }
+    , last: function() {
+        return this.probes[this.probes.length - 1]
+      }
+    , clear: function() {
+        while(this.probes.length > 0) this.probes.pop()
+        return this
+      }
+    }
+
+//debug
+require("request-debug")(request)
+//request.debug = true
+
+probe.add(getRequest).times(2)
 
 server.listen(port, function() {
-  whenServerIsReady.forEach(function(fn) { fn() })
+  probe.probes.forEach(function(fn) { fn() })
 })
 
 function getRequest() {
@@ -35,15 +62,11 @@ function getRequest() {
     if (err) console.log(err), process.exit(1)
 
   //  console.dir(res)
-    console.log("*******************************************\r\n")
     console.log('GET ' + uri)
     console.dir(res.headers)
+    console.log("*******************************************\r\n")
   });
 }
-
-[1,2].forEach(function() {
-  whenServerIsReady.push(getRequest)
-})
 
 setTimeout(function() {
   server.close()
